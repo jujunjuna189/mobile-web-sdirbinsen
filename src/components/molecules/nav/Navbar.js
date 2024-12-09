1b29e729139fad3bfd1dd10b50e844aea7c257a6
Jujun Juna189 <53271616+jujunjuna189@users.noreply.github.com>
2024-12-09 09:15:51 +0700

    Update

--- src/components/molecules/nav/Navbar.js
+++ src/components/molecules/nav/Navbar.js
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { logLogo } from "../../../assets";
+import { RouterName } from "../../../utils";
 
 const Navbar = () => {
     const navigation = useNavigate();
     return (
         <div>
             <div className="flex justify-between py-2 px-3">
-                <div className="leading-3 flex gap-3 cursor-pointer" onClick={() => navigation("/")}>
+                <div className="leading-3 flex gap-3 cursor-pointer" onClick={() => navigation(RouterName.board)}>
                     <img src={logLogo} alt="logo gunner smart app" className="rounded-md shadow-all-yellow" width={40} />
                     <span className="font-semibold text-2xl text-[#F9FF3F]">Gunner Smart App</span>
                 </div>
