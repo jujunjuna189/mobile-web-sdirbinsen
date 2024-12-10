1b66ee2678b2c6fcc5c7b339121492cf30e7a2e2
Jujun Juna189 <53271616+jujunjuna189@users.noreply.github.com>
2024-12-11 02:02:57 +0700

    Update

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -1,6 +1,7 @@
-import { bnrName, icBase, icDataGathering, icInfantry, icOnlineLesson, icTank, icWarning, imgMainBg } from "../../assets";
+import { bnrName, icBase, icDataGathering, icInfantry, icOnlineLesson, icProcess, icTank, icWarning, imgMainBg } from "../../assets";
 import { Content, Navbar } from "../../components";
 import { UseBoardContext } from "../../contexts/board/BoardContext";
+import { getLocalUser } from "../../service/LocalStorage";
 import { RouterName } from "../../utils";
 
 const BoardPage = () => {
@@ -17,67 +18,89 @@ const BoardPage = () => {
                     <img src={bnrName} alt="Banner gunner smart application" />
                 </div>
                 <div className="grid grid-cols-3 gap-5 px-3 mt-6 justify-center">
-                    <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.trakorps)}>
-                        <div className=" h-full bg-[#4B7D5E]">
-                            <div className="flex justify-center items-center py-1">
-                                <img src={icBase} alt="icon" className="w-[55px] -mb-2" />
-                            </div>
-                            <div className="text-center">
-                                <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>TRAKORPS</strong>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binman)}>
-                        <div className="h-full bg-[#4B7D5E]">
-                            <div className="flex justify-center items-center py-1">
-                                <img src={icInfantry} alt="icon" className="w-[55px] -mb-2" />
-                            </div>
-                            <div className="text-center">
-                                <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINMAN</strong>
+                    {getLocalUser()?.permission?.["view.trakorps"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.trakorps)}>
+                            <div className=" h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icBase} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>TRAKORPS</strong>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binmat)}>
-                        <div className="h-full bg-[#4B7D5E]">
-                            <div className="flex justify-center items-center py-1">
-                                <img src={icTank} alt="icon" className="w-[55px] -mb-2" />
-                            </div>
-                            <div className="text-center">
-                                <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINMAT</strong>
+                    )}
+                    {getLocalUser()?.permission?.["view.binman"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binman)}>
+                            <div className="h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icInfantry} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINMAN</strong>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
+                    {getLocalUser()?.permission?.["view.binmat"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binmat)}>
+                            <div className="h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icTank} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINMAT</strong>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {getLocalUser()?.permission?.["view.binsiapsat"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binsiapsat)}>
+                            <div className="h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icDataGathering} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINSIAPSAT</strong>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {getLocalUser()?.permission?.["view.learning"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.bukuPintar)}>
+                            <div className="h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icOnlineLesson} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BUKU PINTAR</strong>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {getLocalUser()?.permission?.["view.referensi"] && (
+                        <div className="h-[5.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => { }}>
+                            <div className="h-full bg-[#4B7D5E]">
+                                <div className="flex justify-center items-center py-1">
+                                    <img src={icProcess} alt="icon" className="w-[55px] -mb-2" />
+                                </div>
+                                <div className="text-center">
+                                    <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>REFERENSI</strong>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                <div className="flex justify-center gap-5 px-3 mt-6">
-                    <div className="h-[5.5rem] w-[6.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.binsiapsat)}>
-                        <div className="h-full bg-[#4B7D5E]">
-                            <div className="flex justify-center items-center py-1">
-                                <img src={icDataGathering} alt="icon" className="w-[55px] -mb-2" />
-                            </div>
-                            <div className="text-center">
-                                <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BINSIAPSAT</strong>
+                {announcement.length > 0 && (
+                    <div className="mt-8 flex justify-center">
+                        <div className="bg-[#4B7D5E] flex justify-center py-1 pr-3 pl-1 rounded-md">
+                            <div className="flex gap-1 items-center">
+                                <img src={icWarning} alt="icon" className="w-10 -mb-2" />
+                                <span className="text-white text-[24px] font-extrabold" style={{ textShadow: "0px 1px 3px #000000" }}>Troop Info!</span>
                             </div>
                         </div>
                     </div>
-                    <div className="h-[5.5rem] w-[6.5rem] rounded-md bg-gradient-to-r from-[#A49F1D] via-[#FFFDBC] to-[#CFB20E] p-[0.20rem]" onClick={() => navigation(RouterName.bukuPintar)}>
-                        <div className="h-full bg-[#4B7D5E]">
-                            <div className="flex justify-center items-center py-1">
-                                <img src={icOnlineLesson} alt="icon" className="w-[55px] -mb-2" />
-                            </div>
-                            <div className="text-center">
-                                <strong className="text-[#D6D156] font-black" style={{ textShadow: "0px 1px 3px #000000" }}>BUKU PINTAR</strong>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-8 flex justify-center">
-                    <div className="bg-[#4B7D5E] flex justify-center py-1 pr-3 pl-1 rounded-md">
-                        <div className="flex gap-1 items-center">
-                            <img src={icWarning} alt="icon" className="w-10 -mb-2" />
-                            <span className="text-white text-[24px] font-extrabold" style={{ textShadow: "0px 1px 3px #000000" }}>Troop Info!</span>
-                        </div>
-                    </div>
-                </div>
+                )}
                 <div className="px-3 my-3">
                     {announcement?.data?.map((item, index) => {
                         return (
